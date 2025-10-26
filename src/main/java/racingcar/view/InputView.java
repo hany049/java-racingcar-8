b27ce8f78d085f372b2c55fa105f7c0da527efb4
hany049 <kdw6034@gmail.com>
2025-10-26 22:13:58 +0900

    feat: 입력 받는 기능

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,21 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class InputView {
+    private static final String INPUT_CARNAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRIAL ="시도할 횟수는 몇 회인가요?";
+
+    public static String getCarName() {
+        System.out.println(INPUT_CARNAME);
+        return Console.readLine();
+    }
+
+    public static String getTrialNumber() {
+        System.out.println(INPUT_TRIAL);
+        return Console.readLine();
+    }
+
+}
+
